a7d79a6518a2a51420ec0b7ed4f555437c46abf6
InsanelyAvner <avner.keinan@gmail.com>
2024-09-22 20:50:13 +0800

    r

--- components/env-var-warning.tsx
+++ components/env-var-warning.tsx
@@ -6,7 +6,7 @@ export function EnvVarWarning() {
   return (
     <div className="flex gap-4 items-center">
       <Badge variant={"outline"} className="font-normal">
-        Please do not abuse this service
+        Please use responsibly
       </Badge>
     </div>
   );
